25bbfb4820f08ed0c309f8d11dbab4db6c2448de
Vaishnavi77770 <vaishnavib.ai23@bmsce.ac.in>
2025-05-19 15:24:20 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,14 @@
+// File: A.java (Package: apack)
+package apack;
+
+public class A {
+
+    protected int defaultVar = 10;
+    protected int protectedVar = 20;
+    private int privateVar = 30;
+    public int publicVar = 40;
+
+    public A() {
+        System.out.println("Class A Constructor Called");
+    }
+}
